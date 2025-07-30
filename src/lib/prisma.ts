109540f70d6e127694e601c9b3c52f50e553f99b
kayleebm <144858742+kayleebm@users.noreply.github.com>
2025-07-29 18:54:48 -1000

    trying to fix vercel again

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,19 @@
 import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+//
+// Learn more:
+// https://pris.ly/d/help/next-js-best-practices
 
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+// eslint-disable-next-line import/prefer-default-export, operator-linebreak
 export const prisma =
-  globalForPrisma.prisma ??
+  // eslint-disable-next-line operator-linebreak
+  globalForPrisma.prisma ||
   new PrismaClient({
-    log: ['query'],
+    log: ['query'], // CAM: is this the right level of logging?
   });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
